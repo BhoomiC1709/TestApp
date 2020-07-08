6c6dace0636986385b76bd8adc24db4e68d7189b
Chirag Vaghela <chiragvaghela.me@gmail.com>
2020-07-07 22:18:54 -0400

    First commit

---
+++ testApp/customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testApp
+{
+    public class customer
+    {
+        // properties 
+        public int customerId { get; private set; }
+        public string emailAddress { get; set; }
+        public string Firstname { get; set; }
+
+        private string _Lastname; // backing code
+        public string Lastname
+        {
+            get
+            {
+                return _Lastname;
+            }
+            set 
+            {
+                _Lastname = value;
+            }
+        }
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(Lastname)) isValid = false;
+            if (string.IsNullOrWhiteSpace(emailAddress)) isValid = false;
+
+            return isValid;
+        }
+    }
+    
+}
